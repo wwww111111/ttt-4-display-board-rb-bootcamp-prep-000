76745eb329205905f1035101fe45c8155a64c98b
patient-computer-3602 <thelionking898@gmail.com>
2020-03-21 17:44:54 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,25 +9,25 @@ def display_board(board)
   puts [" #{board[6]} | #{board[7]} | #{board[8]} "]
 end 
 
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-display_board(board)
+# board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+# display_board(board)
 
-board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
-display_board(board)
+# board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+# display_board(board)
 
-board = [" ", " ", "O ", " ", " ", " ", " ", " ", " "]
-display_board(board)
-board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
-display_board(board)
+# board = [" ", " ", "O ", " ", " ", " ", " ", " ", " "]
+# display_board(board)
+# board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+# display_board(board)
 
-board = [" ", " ", " ", " ", " ", " ", "O", "O", "O"]
-display_board(board)
+# board = [" ", " ", " ", " ", " ", " ", "O", "O", "O"]
+# display_board(board)
 
-board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
-display_board(board)
+# board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
+# display_board(board)
 
-board = [" ", " ", "O", " ", "O", " ", "O", " ", " "]
-display_board(board)
+# board = [" ", " ", "O", " ", "O", " ", "O", " ", " "]
+# display_board(board)
 
-board = ["", " ", "O", " ", "O", " ", "O", " ", " "]
-display_board(board)
+# board = ["", " ", "O", " ", "O", " ", "O", " ", " "]
+# display_board(board)
